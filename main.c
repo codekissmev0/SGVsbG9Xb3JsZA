da50976bf9720ed0ed6c2d951a143984118ea34f
codekissmev0 <278903984+codekissmev0@users.noreply.github.com>
2026-06-28 19:31:14 +0800

    设置C语言编译的目标标准以及MSVC和MinGW的一点区别

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int i;
+    for (i = 0; i < 5; ++i) {
+    // for (int i = 0; i < 5; ++i) { //c99才开始支持这种写法,如果set(CMAKE_C_STANDARD 90),你用MinGW则不支持, 但vs支持（因为它是msvc Microsoft Visual C++ 本质是个c++编译器,C++是可以这样写的）
+        printf("Hello, World!\n");
+    }
     return 0;
 }
