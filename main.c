c0629e3f2e695657a9c1d44ac931220be07cb2b8
codekissmev0 <278903984+codekissmev0@users.noreply.github.com>
2026-07-03 20:26:02 +0800

    设置 C23 标准并演示 for 语句内声明变量

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int i;
-    for (i = 0; i < 5; ++i) {
-    // for (int i = 0; i < 5; ++i) { //c99才开始支持这种写法,如果set(CMAKE_C_STANDARD 90),你用MinGW则不支持, 但vs支持（因为它是msvc Microsoft Visual C++ 本质是个c++编译器,C++是可以这样写的）
+    // int i;
+    // for (i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i) { //c99才开始支持这种写法,如果set(CMAKE_C_STANDARD 90),你用MinGW则不支持, 但vs支持（因为它是msvc Microsoft Visual C++ 本质是个c++编译器,C++是可以这样写的）
         printf("Hello, World!\n");
     }
     return 0;
